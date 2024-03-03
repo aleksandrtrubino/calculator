19ad4cf737746511314b09edee582c20bd59510f
aleksandrtrubino <trubino2003@gmail.com>
2024-03-03 14:52:53 +0300

    Initial commit

---
+++ calculator_module/src/main/java/ru/vistar/trubino/Calculator.java
@@ -0,0 +1,7 @@
+package ru.vistar.trubino;
+
+public class Calculator {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
---
+++ main_module/src/main/java/ru/vistar/trubino/Main.java
@@ -0,0 +1,7 @@
+package ru.vistar.trubino;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    }
+}
